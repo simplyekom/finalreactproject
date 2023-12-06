9b686f3f612577b5f269d96dd1de746280bfd740
simplyekom <simplyekom@gmail.com>
2023-12-05 20:53:45 -0700

    images for popular picks added

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 
 function Popular() {
@@ -13,18 +14,41 @@ function Popular() {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`)
     const data = await api.json();
     setPopular(data.recipes)
+    console.log(data.recipes)
   }
   return (
     <div>
       {popular.map((recipe) => {
         return(
-          <div key={recipe.id}> 
-            <p> {recipe.title}</p>
-          </div>
+          <Wrapper>
+            <h3>
+              Popular Picks
+            </h3>
+            {popular.map((recipe) => {
+              return(
+
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+
+              );
+            })}
+          </Wrapper>
         );
       })}
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+`
+
 
 export default Popular
