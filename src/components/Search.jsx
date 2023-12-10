1b2d27ef5e6275b08a0dde7cbfbdfe656371c9eb
simplyekom <simplyekom@gmail.com>
2023-12-09 23:08:53 -0700

    search component and page completed

---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import {useNavigate} from 'react-router-dom';
+
+function Search() {
+    const [input, setInput] = useState("");
+    const navigate = useNavigate();
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate('/searched/' + input)
+    };
+  return (
+    <FormStyle onSubmit={submitHandler}>
+        <div>
+            <FaSearch></FaSearch>
+            <input onChange={(e) => setInput(e.target.value)}type="text" value={input} />
+        </div>
+    </FormStyle>
+  )
+}
+
+
+const FormStyle = styled.form`
+    margin: 0rem 20rem;
+        div {
+            position: relative;
+            width: 100%;
+        }
+    position: relative;
+    width: 100%;
+    input {
+        border: none;
+        background: white;
+        color: black;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 45%;
+    }
+    svg{
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        
+    }
+`
+export default Search
