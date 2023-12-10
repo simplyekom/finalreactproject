bb4ea3b10c5807f8fe3e5f10793c6042f265d7fb
simplyekom <simplyekom@gmail.com>
2023-12-10 00:03:02 -0700

    recipe page in progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
 
+
 function App() {
   return (
     <div className="App">
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import {Splide, SplideSlide} from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from "react-router-dom";
 
 
 function Popular() {
@@ -46,8 +47,10 @@ function Popular() {
                 <SplideSlide key={recipe.id}>
 
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                  </Link>
                 </Card>
 
                 </SplideSlide>
